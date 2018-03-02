32fd49fe658d56ef954cb6e1cb721039f7100796
SavhannaMcLellan <savhanna.mclellan@mail.utoronto.ca>
2018-03-01 22:38:08 -0500

    Added new FoodMods to the menu with ingrediets if needed. Also refactored burger item to hamburger.

--- phase1/src/Menu.java
+++ phase1/src/Menu.java
@@ -16,19 +16,35 @@ public class Menu {
         Ingredient hotdog = new IngredientImpl("Hotdog");
         Ingredient hotdogBun = new IngredientImpl("Hotdog Bun");
 
-        List<Ingredient> burger = Arrays.asList(burgerBun, patty, lettuce, tomato);
+        List<Ingredient> hamburger = Arrays.asList(burgerBun, patty, lettuce, tomato);
         List<Ingredient> chickenBurger = Arrays.asList(burgerBun, chickenPatty, lettuce, tomato);
         List<Ingredient> vegetarian = Arrays.asList(burgerBun, veg, lettuce, tomato);
         List<Ingredient> fries = Arrays.asList(potato, salt);
         List<Ingredient> hot = Arrays.asList(hotdog, hotdogBun);
 
-        menu[0] = new MenuItemImpl("Hamburger", 5, burger);
+        menu[0] = new MenuItemImpl("Hamburger", 5, hamburger);
         menu[1] = new MenuItemImpl("Chicken Burger", 4.5, chickenBurger);
         menu[2] = new MenuItemImpl("Veggie Burger", 4, vegetarian);
         menu[3] = new MenuItemImpl("Fries", 1.50, fries);
         menu[4] = new MenuItemImpl("Hotdog", 3, hot);
 
+        // Addon items
+        Ingredient ketchup = new IngredientImpl("Ketchup");
+        Ingredient mustard = new IngredientImpl("Mustard");
+        Ingredient sentience = new IngredientImpl("Sentience");
+        Ingredient cheese = new IngredientImpl("Cheese");
+        Ingredient bacon = new IngredientImpl("Bacon");
+        FoodMod addTomato = new FoodModImpl(tomato, 0.25);
+        FoodMod addLettuce = new FoodModImpl(lettuce, 0.25);
+        FoodMod addSalt = new FoodModImpl(salt, 0.00);
+        FoodMod addketchup = new FoodModImpl(ketchup, 0.00);
+        FoodMod addMustard = new FoodModImpl(mustard, 0.00);
+        // Should only be added to items ordered by rude customers in need of a good scare.
+        FoodMod addSentience = new FoodModImpl(sentience, 6.66);
+        FoodMod addCheese = new FoodModImpl(cheese, 1.00);
+        FoodMod addBacon = new FoodModImpl(bacon, 2.00);
     }
+
     public void printMenu(){
         for(MenuItem item : menu){
             System.out.println(item.getName() + "\nIngredients:" + item.printIngredients() +"\n");
