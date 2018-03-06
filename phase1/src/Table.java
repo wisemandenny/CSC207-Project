9a1cea0881e181078d02edb59396be2a060496c0
Denny Wiseman <d.wiseman@utoronto.ca>
2018-03-06 18:06:59 -0500

    Miscellaneous small changes

--- phase1/src/Table.java
+++ phase1/src/Table.java
@@ -1,40 +1,41 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Table {
-    private int id;
-    private List<MenuItem> bill = new ArrayList<>();
-    private Order order;
+	private final int id;
+	private final List<MenuItem> bill = new ArrayList<>();
+	private Order order;
 
-    Table(int id) {
-        this.id = id;
-    }
+	Table(int id) {
+		this.id = id;
+	}
 
-    void addOrderToTable(final Order o){
-        order = o;
-    }
+	void addOrderToTable(Order o) {
+		order = o;
+	}
 
-    void addToBill(final Order o) { bill.addAll(o.getItems()); }
+	void addToBill(Order o) {
+		bill.addAll(o.getItems());
+	}
 
-    void printBill() {
-        System.out.println("BILL FOR TABLE #" + id);
-        for(MenuItem item : bill) {
-            System.out.println(item.getQuantity() + " " + item.getName() + ": $" + item.getPrice());
-        }
-        System.out.println("Total: $" + getBillPrice() + "\n");
-    }
+	void printBill() {
+		System.out.println("BILL FOR TABLE #" + id);
+		for (MenuItem item : bill) {
+			System.out.println(item.getQuantity() + " " + item.getName() + ": $" + item.getPrice());
+		}
+		System.out.println("Total: $" + getBillPrice() + "\n");
+	}
 
-    private double getBillPrice(){
-        double ret = 0.00;
-        for(MenuItem item: bill){
-            ret += item.getPrice() * item.getQuantity();
-            ret += item.getModPrice();
-        }
-        return ret;
-    }
+	private double getBillPrice() {
+		double ret = 0.00;
+		for (MenuItem item : bill) {
+			ret += item.getPrice() * item.getQuantity();
+			ret += item.getModPrice();
+		}
+		return ret;
+	}
 
-    protected Order getOrder(){
-        return order;
-    }
+	protected Order getOrder() {
+		return order;
+	}
 }
