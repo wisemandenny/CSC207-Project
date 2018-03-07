aa1e7470d8a934ba67bb77c9c699d6657d00589f
Denny Wiseman <wisemandenny@gmail.com>
2018-03-07 11:45:59 -0500

    EventManager: removed magic numbers and replaced them with named constants
    Event: removed magic numbers and replaced them with named constants
    in getter methods which return reference types, replaced the actual reference with a copy to avoid exposing private internal state to the world

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Event {
+    private static final int QUANTITY_ADDRESS = 0;
+    private static final int NAME_ADDRESS = 1;
     private final EventType type;
     private final int tableId;
     private Order order;
@@ -32,16 +35,6 @@ class Event {
         commentSetter(commentList);
     }
 
-    private void itemBuilder(String strings){
-        this.deduction = new ArrayList<>();
-        for(String item: strings.split(",\\s")){
-            String[] itemSplitString = item.split("\\s", 2);
-            int quantity = Integer.parseInt(itemSplitString[0]);
-            String itemName = itemSplitString[1];
-            // make a menu item and add it to the deduction list
-            deduction.add(new MenuItemImpl(itemName, quantity));
-        }
-    }
 
     private void commentSetter(String commentList){
         int count = 0;
@@ -49,30 +42,41 @@ class Event {
         String[] comments = new String[deduction.size()];
         // add every comment to the comment list
         for(String itemComment: commentList.split(",\\s")){
-            comments[count] = itemComment;
-            count ++;
+            comments[count++] = itemComment;
         }
         // assign each comment to each returned menu item
         count = 0;
         for(MenuItem item: deduction){
-            item.setComment(comments[count]);
-            count++;
+            item.setComment(comments[count++]);
+        }
+    }
+
+    private void itemBuilder(String strings){
+        this.deduction = new ArrayList<>();
+        if(strings.length() < 2) throw new IllegalArgumentException("Invalid strings length in Event.itemBuilder");
+        for(String item: strings.split(",\\s")){
+           addItemToList(item, deduction);
         }
     }
 
     private void orderConstructorHelper(String strings){
         List<MenuItem> orderItems = new ArrayList<>();
         for (String item : strings.split(",\\s")) { //split the order into [1-9] <item name> substrings
-            String[] orderItemSplitString = item.split("\\s", 2);
-            int quantity = Integer.parseInt(orderItemSplitString[0]);
-            String name = orderItemSplitString[1];
-            orderItems.add(new MenuItemImpl(name, quantity));
+            addItemToList(item, orderItems);
         }
         order = new OrderImpl(orderItems);
     }
 
+    private void addItemToList(String item, List<MenuItem> list){
+        String[] itemSplitString = item.split("\\s", 2);
+        int quantity = Integer.parseInt(itemSplitString[QUANTITY_ADDRESS]);
+        String itemName = itemSplitString[NAME_ADDRESS];
+        // make a menu item and add it to the deduction list
+        list.add(new MenuItemImpl(itemName, quantity));
+    }
+
     EventType getType() {
-        return type;
+        return EventType.fromString(type.toString());
     }
 
     String getAddOn() {
@@ -80,12 +84,12 @@ class Event {
     }
 
     Order getOrder() {
-        return order;
+        return new OrderImpl(order.getItems());
     }
 
     int getTableId() {
         return tableId;
     }
 
-    List<MenuItem> getDeductions() { return deduction; }
+    List<MenuItem> getDeductions() { return new ArrayList<>(deduction); }
 }
