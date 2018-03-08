042f6f43854bcc30ae0460b2fa0cf52499073f0f
Denny Wiseman <d.wiseman@utoronto.ca>
2018-03-07 22:59:15 -0500

    Refactored foodmods into ingredients, had to update almost all files. Currently the modifications do not display

--- phase1/src/Menu.java
+++ phase1/src/Menu.java
@@ -1,11 +1,5 @@
-import java.util.Set;
-
 public interface Menu {
-    FoodMod getFoodMod(FoodMod query);
-
     MenuItem[] getMenu();
 
     MenuItem getMenuItem(MenuItem item);
-
-    Set<FoodMod> getModsMenu();
 }
