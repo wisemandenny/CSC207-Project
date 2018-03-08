7561af863ab1b071d60c78a7879e5dff297fef30
Milan Miladinovic <milan.miladinovic@mail.utoronto.ca>
2018-03-08 16:43:19 -0500

    Added javadocs to EventType

--- phase1/src/EventType.java
+++ phase1/src/EventType.java
@@ -7,7 +7,12 @@ public enum EventType {
     SERVERRETURNED("serverReturned");
 
     private final String type;
-
+    /**
+     * Returns the eventType that corresponds to the given String type.
+     *
+     * @param type the String representation of an EventType
+     * @return      an EventType generated that corresponds to the given String
+     */
     EventType(String type) {
         this.type = type;
     }
@@ -21,6 +26,9 @@ public enum EventType {
         throw new IllegalArgumentException("Unrecognized type " + type);
     }
 
+    /**
+     *{@inheritDoc}
+     */
     @Override
     public String toString() {
         return type;
