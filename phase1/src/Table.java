72a3a4c844105f627730a36cbf127f885fc5fa8b
Denny Wiseman <d.wiseman@utoronto.ca>
2018-03-08 10:41:34 -0500

    Reverted several changes on Table.java which broke functionality

--- phase1/src/Table.java
+++ phase1/src/Table.java
@@ -2,46 +2,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
-    /**
-     * A table at the restaurant with identification, a bill, and sometimes deductions and uncooked foods.
-     */
     private final int id;
     private final List<MenuItem> bill = new ArrayList<>();
     private final List<MenuItem> deductions = new ArrayList<>();
-    private final List<MenuItem> uncookedMenuitems = new ArrayList<>();
+    private final List<MenuItem> uncookedMenuItems = new ArrayList<>();
     private Order order;
 
-    Table(int id) {
+    Table(final int id) {
         this.id = id;
     }
 
-    public void addUncookedMenuitems(MenuItem item) {
+    public void addUncookedMenuitems(final MenuItem item) {
         uncookedMenuItems.add(item);
     }
 
     public List<MenuItem> getUncookedMenuItems() {
-        return uncookedMenuitems;
+        return uncookedMenuItems;
     }
 
-    void addOrderToTable(Order o) {
+    void addOrderToTable(final Order o) {
         order = o;
     }
 
-    void addToBill(Order o) {
-        /**
-         * Adds the items from order o to this tables' bill.
-         */
+    void addToBill(final Order o) {
         bill.addAll(o.getItems());
     }
 
-    void addToDeductions(List<MenuItem> items) {
+    void addToDeductions(final MenuItem item, final int quantity, final String comment) {
+        final MenuItem itemToAdd = new MenuItemImpl(item, quantity);
+        itemToAdd.setComment(comment);
+
+        double cost = item.getPrice();
+        cost *= item.getQuantity() * -1;
+        itemToAdd.setPrice(cost);
+        deductions.add(itemToAdd);
+
+        for (final Ingredient mod : itemToAdd.getRemovedIngredients()) {
+
+        }
+    }
+
+    void addToDeductions(final List<MenuItem> items) {
         deductions.addAll(items);
     }
 
     // get all the items that have been rejected and make them into a string
     String stringDeductions() {
-        StringBuilder ret = new StringBuilder("");
-        for (MenuItem item : deductions) {
+        final StringBuilder ret = new StringBuilder("");
+        for (final MenuItem item : deductions) {
             ret.append(item.getQuantity()).append(" ").append(item.getName()).append(", ").append(item.getPrice()).append("\n");
         }
         return ret.toString();
@@ -49,19 +57,19 @@ public class Table {
 
     void printBill() {
         System.out.println("BILL FOR TABLE #" + id);
-        for (MenuItem item : bill) {
+        for (final MenuItem item : bill) {
             System.out.println(item.getQuantity() + " " + item.getName() + ": $" + String.format("%.2f", item.getPrice()));
-            for (Ingredient addedIng : item.getExtraIngredients()) {
+            for (final Ingredient addedIng : item.getExtraIngredients()) {
                 System.out.println("add " + item.getQuantity() + " " + addedIng.getName() + ": $" + String.format("%.2f", addedIng.getPrice() * item.getQuantity()));
             }
-            for (Ingredient removedIng : item.getRemovedIngredients()) {
+            for (final Ingredient removedIng : item.getRemovedIngredients()) {
                 System.out.println("remove " + item.getQuantity() + " " + removedIng.getName() + ": -$" + String.format("%.2f", removedIng.getPrice() * item.getQuantity()));
             }
 
         }
         if (!deductions.isEmpty()) {
             System.out.println("\nDEDUCTIONS (-)");
-            for (MenuItem item : deductions) {
+            for (final MenuItem item : deductions) {
                 System.out.println(item.getQuantity() + " " + item.getName() + ": $" +
                         String.format("%.2f", item.getPrice()) + " | Reason: " + item.getComment());
             }
@@ -71,12 +79,12 @@ public class Table {
 
     private String getBillPrice() {
         double ret = 0.00;
-        for (MenuItem item : bill) {
+        for (final MenuItem item : bill) {
             ret += item.getPrice();
             ret += item.getExtraIngredientPrice();
         }
         // remove the price of any items that were returned
-        for (MenuItem item : deductions) {
+        for (final MenuItem item : deductions) {
             ret += item.getPrice();
         }
         return String.format("%.2f", ret);
