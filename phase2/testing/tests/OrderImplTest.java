88d8d7ebac54bbd57ac47755b5f9d98d4073079a
Milan Miladinovic <milan.miladinovic@mail.utoronto.ca>
2018-03-16 20:26:09 -0400

    Wrote a test for OrderImplTest

--- phase2/testing/tests/OrderImplTest.java
+++ phase2/testing/tests/OrderImplTest.java
@@ -1,7 +1,79 @@
 // TODO: write these tests
 package tests;
+import org.junit.Test;
 import restaurant.*;
+import menu.*;
+
+
+import static org.junit.Assert.*;
 
 public class OrderImplTest {
 
+    @Test
+    public void testGetItems(){
+        // menu
+        Menu menu = new BurgerMenu();
+        // order
+        Order order = new OrderImpl("1 Hamburger / +Ketchup -Bacon, 1 Hamburger, 1 Coke");
+        assertTrue(order.getItems().size() == 3);
+
+        // Check if there is a plain hamburger in the inventory
+        MenuItem burger = menu.getMenuItem("Hamburger");
+        assertTrue(order.getItems().contains(burger));
+
+        // Create a Hamburger with mods
+        MenuItem modBurger = menu.getMenuItem("Hamburger");
+
+        // Ingredients for the modBurger
+        Ingredient ketchup = IngredientFactory.makeIngredient("Ketchup");
+        Ingredient bacon = IngredientFactory.makeIngredient("Bacon");
+        modBurger.addExtraIngredient(ketchup);
+        modBurger.removeIngredient(bacon);
+
+        // Check if the modBurger has been added
+        assertTrue(order.getItems().contains(modBurger));
+
+        // Check if a Coke has been added.
+        assertTrue(order.getItems().contains(menu.getMenuItem("Coke")));
+    }
+
+    @Test
+    public void testAdd() {
+
+    }
+
+    @Test
+    public void testRemove() {
+
+    }
+
+    @Test
+    public void testGetId() {
+
+    }
+
+    @Test
+    public void testGetTableId() {
+
+    }
+
+    @Test
+    public void testReturned() {
+
+    }
+
+    @Test
+    public void testGetIngredients() {
+
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+
+    @Test
+    public void testOrderStringParser() {
+
+    }
 }
