ec9fb2dbe889b4b53efbc25d18e506d68c2b8229
sanch187 <saquiles0@gmail.com>
2018-03-21 01:26:16 -0400

    -setup testRemoveFromInventory for testing (assert functions not implemented yet)

--- phase2/tests/InventoryImplTest.java
+++ phase2/tests/InventoryImplTest.java
@@ -2,16 +2,22 @@
 import menu.BurgerMenu;
 import menu.Ingredient;
 import menu.Menu;
+import menu.MenuItem;
 
 import org.junit.Test;
 
 import static menu.IngredientFactory.makeIngredient;
+import static menu.MenuItemFactory.makeMenuItem;
 import static org.junit.Assert.*;
 import static restaurant.InventoryFactory.makeInventory;
+import static restaurant.OrderFactory.makeOrder;
 
 import restaurant.Inventory;
+import restaurant.Order;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class InventoryImplTest {
@@ -25,11 +31,18 @@ public class InventoryImplTest {
         Map<Ingredient, Integer> mocShip = new HashMap<>();
         mocShip.put(i, 1);
         inventory.addToInventory(mocShip);
+
         assertTrue(currValue == inventory.getContents().get(i) - 1);
     }
 
     @Test
     public void testRemoveFromInventory(){
+        Inventory inventory = makeInventory(menu);
+        List<MenuItem> items = new ArrayList<>();
+        List<Ingredient> ingredients = new ArrayList<>();
+        MenuItem item = makeMenuItem("Nona's pizza", 100.00, ingredients);
+        items.add(item);
+        Order o = makeOrder(items, 19, 77);
     }
 
     @Test
