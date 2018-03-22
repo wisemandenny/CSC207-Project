c3523f9922cab7233cfb11d11d9db01edf8af195
sanch187 <saquiles0@gmail.com>
2018-03-22 03:39:23 -0400

    -created class MyLogger with methods getLogger and log -The functionality of this class is to log events to a txt from multiple classes (not yet implemented)

---
+++ phase2/gui/src/main/MyLogger.java
@@ -0,0 +1,74 @@
+package src.main;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class MyLogger {
+    //Used this webpage for reference http://hanoo.org/index.php?article=how-to-generate-logs-in-java
+    static Logger logger;
+    //file handler
+    public Handler fh;
+    Formatter plainText;
+
+    private  MyLogger() throws IOException{
+        logger = Logger.getLogger(MyLogger.class.getName());
+        fh = new FileHandler("myLog.txt", true);
+        plainText =  new SimpleFormatter();
+        fh.setFormatter(plainText);
+        logger.addHandler(fh);
+    }
+
+    private static Logger getLogger(){
+        if(logger == null){
+            try {
+                new MyLogger();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return logger;
+    }
+    public static void log(Level level, String msg){
+        getLogger().log(level, msg);
+        System.out.println(msg);
+    }
+
+    //this code is for writing logs to a text file.
+    /**
+    public static void main(String[] args) {
+
+
+         Logger logger = Logger.getLogger("MyLog");
+         FileHandler fh;
+
+         try {
+
+         // This block configure the logger with handler and formatter
+         fh = new FileHandler("C:/temp/test/MyLogFile.log");
+         logger.addHandler(fh);
+         SimpleFormatter formatter = new SimpleFormatter();
+         fh.setFormatter(formatter);
+
+         // the following statement is used to log any messages
+         logger.info("My first log");
+
+         } catch (SecurityException e) {
+         e.printStackTrace();
+         } catch (IOException e) {
+         e.printStackTrace();
+         }
+
+         logger.info("Hi How r u?");
+
+    }
+     */
+
+    //this code is what will be called from classes to log an event.
+    /**
+     public class Test1 {
+        public Test1()throws IOException {
+            MyLogger.log(Level.INFO, MyLogger.class.getName());
+        }
+     }
+     */
+}
