32dd497acabe33aef66fab30a06717e4939ec321
sanch187 <saquiles0@gmail.com>
2018-03-22 22:00:54 -0400

    -Completed testRemoveFromInventory and testPrintContents. -Duplication problem in actual output of testPrintContents.

--- phase2/tests/InventoryImplTest.java
+++ phase2/tests/InventoryImplTest.java
@@ -15,6 +15,8 @@ import static restaurant.OrderFactory.makeOrder;
 import restaurant.Inventory;
 import restaurant.Order;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,27 +39,45 @@ public class InventoryImplTest {
 
     @Test
     public void testRemoveFromInventory(){
+        //This block of code tests the second instance of removeFromInventory
         Inventory inventory = makeInventory(menu);
+        Ingredient ingredient = makeIngredient("Tomato");
+        int currValue = inventory.getContents().get(ingredient);
+        inventory.removeFromInventory(ingredient);
+
+        assertTrue(inventory.getContents().get(ingredient) == currValue - 1);
+        assertFalse(inventory.getContents().get(ingredient) == currValue);
+
+        //This block of code tests the first instance of removeFromInventory
         List<MenuItem> items = new ArrayList<>();
         List<Ingredient> ingredients = new ArrayList<>();
-        MenuItem item = makeMenuItem("Nona's pizza", 100.00, ingredients);
+        ingredients.add(makeIngredient("Tomato"));
+        MenuItem item = makeMenuItem("Hamburger", 100.00, ingredients);
         items.add(item);
         Order o = makeOrder(items, 19, 77);
+        currValue = inventory.getContents().get(ingredient);
+        inventory.removeFromInventory(o);
+
+        assertTrue(inventory.getContents().get(ingredient) == currValue - 1);
+        assertFalse(inventory.getContents().get(ingredient) == currValue);
     }
 
     @Test
-    public void testGetContents(){
+    public void testMakeRestockRequest (){
     }
 
     @Test
-    public void testMakeRestockRequest(){
+        public void testPrintContents(){
+        Inventory inventory = makeInventory(menu);
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        inventory.printContents();
+        String expectedOutput = "";
+
+        for(Map.Entry<Ingredient, Integer> entry : inventory.getContents().entrySet()){
+            expectedOutput += "Name: " +entry.getKey().getName() + "       Quantity: " + entry.getValue() + "\n";
+        }
+
+        assertEquals(expectedOutput, outContent.toString());
     }
-
-    @Test
-    public void testPrintContents(){
-    }
-
-
-
-
 }
