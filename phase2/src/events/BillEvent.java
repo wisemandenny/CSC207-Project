f524751bd263659a9a5571f794ac23661fb4d749
sanch187 <saquiles0@gmail.com>
2018-03-24 23:48:46 -0400

    Method Do event logs event

--- phase2/src/events/BillEvent.java
+++ phase2/src/events/BillEvent.java
@@ -1,6 +1,9 @@
 package events;
 
+import restaurant.RestaurantLogger;
 import restaurant.Table;
+
+import java.util.logging.Level;
 
 public class BillEvent implements Event {
     private final Table table;
@@ -15,6 +18,7 @@ public class BillEvent implements Event {
     public void doEvent() {
 //        table.printBill();
         table.getSeat(seat).printBill();
+        RestaurantLogger.log(Level.INFO, table.toString() + "billed");
     }
 
     @Override
