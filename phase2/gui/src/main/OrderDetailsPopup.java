1dec79e8bb7f6b5f4ff0c51052b3f2cccfca6106
Denny Wiseman <d.wiseman@utoronto.ca>
2018-03-26 01:23:09 -0400

    cleaned up sizing issues in order details popup

---
+++ phase2/gui/src/main/OrderDetailsPopup.java
@@ -0,0 +1,123 @@
+package main;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.*;
+import menu.Ingredient;
+import restaurant.Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class OrderDetailsPopup {
+    StackPane parent;
+    List<menu.MenuItem> orderItems = new ArrayList<>();
+
+    OrderDetailsPopup(StackPane parent, List<JFXButton> selectedItemButtons){
+        this.parent = parent;
+        loadAddDialog(selectedItemButtons);
+        for(JFXButton itemButton : selectedItemButtons){
+            orderItems.add(Restaurant.getInstance().getMenu().getMenuItem(itemButton.getText()));
+        }
+    }
+
+    private void changeItemQuantity(MenuButton dropdownMenu, int quantity, String itemName){
+        dropdownMenu.setText(String.valueOf(quantity));
+        menu.MenuItem updateItem = Restaurant.getInstance().getMenu().getMenuItem(itemName);
+        orderItems.get(orderItems.indexOf(updateItem)).setQuantity(quantity);
+        orderItems.forEach(e -> System.out.println(e.getQuantity() + " " + e.getName()));
+    }
+
+    private HBox makeItemHBox(JFXButton button, JFXListView extras, JFXListView removed){
+        HBox itemBox = new HBox();
+        Region filler = new Region();
+        HBox.setHgrow(filler, Priority.ALWAYS);
+        MenuButton quantitySelector = new MenuButton("Select Quantity");
+        for (int i = 1; i < 11; i++) {
+            final int j = i;
+            MenuItem quantity = new MenuItem(String.valueOf(j));
+            quantity.setOnAction(e -> changeItemQuantity(quantitySelector, j, button.getText())); //will need more here
+            quantitySelector.getItems().add(quantity);
+        }
+        itemBox.getChildren().addAll(quantitySelector, filler, new Label(button.getText()));
+        itemBox.setOnMouseClicked(e -> loadModLists(Restaurant.getInstance().getMenu().getMenuItem(button.getText()), extras, removed));
+        return itemBox;
+    }
+
+    private void loadModLists(menu.MenuItem item, JFXListView<JFXButton> extras, JFXListView<JFXButton> removed){
+        List<Ingredient> allIngredients = Restaurant.getInstance().getMenu().getAllIngredients();
+        allIngredients.removeAll(item.getIngredients());
+        ObservableList<JFXButton> addableIngredients = FXCollections.observableArrayList();
+        for(Ingredient i : allIngredients){
+            addableIngredients.add(new JFXButton(i.getName()));
+        }
+        extras.setItems(addableIngredients);
+
+        ObservableList<JFXButton> removeableIngredients = FXCollections.observableArrayList();
+        for (Ingredient i : item.getIngredients()){
+            removeableIngredients.add(new JFXButton(i.getName()));
+        }
+        removed.setPrefHeight(extras.getHeight());
+        removed.setItems(removeableIngredients);
+    }
+    private void loadAddDialog(List<JFXButton> selectedItemButtons){
+        //HEADER
+        JFXDialogLayout content = new JFXDialogLayout();
+        Label orderDetailsDialogHeader = new Label("Order Details");
+
+        //PARENT AND LIST COMPONENTS
+        VBox dialogRoot = new VBox(5);
+        dialogRoot.setMinHeight(700);
+        JFXListView<JFXButton> extraIngredientsListView = new JFXListView<>();
+        extraIngredientsListView.setMinHeight(300);
+        JFXListView<JFXButton> removedIngredientsListView = new JFXListView<>();
+        removedIngredientsListView.setMinHeight(300);
+
+
+        //LIST OF SELECTED ITEMS AND THEIR QUANTITIES
+        VBox top = new VBox();
+        JFXListView<HBox> orderItemListView = new JFXListView<>();
+        for (JFXButton button : selectedItemButtons){
+            orderItemListView.getItems().add(makeItemHBox(button, extraIngredientsListView, removedIngredientsListView));
+        }
+        top.getChildren().add(orderItemListView);
+
+        //MOD SELECTION
+        HBox bottom = new HBox();
+        bottom.setMinSize(367, 355);
+        //EXTRAS
+        VBox left = new VBox();
+        left.setMinWidth(185);
+        left.getChildren().add(extraIngredientsListView);
+
+        //INGREDIENTS TO REMOVE
+        VBox right = new VBox();
+        right.setMinWidth(185);
+        right.getChildren().add(removedIngredientsListView);
+
+        //JOIN ALL COMPONENTS
+        bottom.getChildren().addAll(left, right);
+        dialogRoot.getChildren().addAll(top, bottom);
+        JFXButton cancelButton = new JFXButton("Cancel");
+        JFXButton confirmButton = new JFXButton("Confirm Order");
+        content.setHeading(orderDetailsDialogHeader);
+        content.setBody(dialogRoot);
+        content.setActions(cancelButton, confirmButton);
+
+        //BUILD THE DIALOG AND SET THE ACTIONS
+        JFXDialog dialog = new JFXDialog(parent, content, JFXDialog.DialogTransition.CENTER);
+        cancelButton.setOnAction(e -> dialog.close());
+        confirmButton.setOnAction(e -> {
+            //newOrder
+            dialog.close();
+        });
+        dialog.show();
+    }
+}
--- phase2/gui/src/main/ServerView.java
+++ phase2/gui/src/main/ServerView.java
@@ -1,19 +1,11 @@
 package main;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXDialog;
-import com.jfoenix.controls.JFXDialogLayout;
-import com.jfoenix.controls.JFXListView;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.*;
-import menu.Ingredient;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import restaurant.Restaurant;
 
 import java.net.URL;
@@ -30,6 +22,7 @@ public class ServerView extends Observable implements Initializable, Observer{
 
     private BillView billView;
     private DeliverableOrdersView deliverableOrdersView;
+    private OrderDetailsPopup orderDetailsPopup;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -52,7 +45,7 @@ public class ServerView extends Observable implements Initializable, Observer{
 
             List<JFXButton> selectedItemButtons = menuList.getSelectedItems();
 
-            FAB.setOnAction(e -> loadAddDialog(selectedItemButtons));
+            FAB.setOnAction(e -> loadAddDialog(serverViewStackPane, selectedItemButtons));
         } catch (Exception ex) {
             //TODO: add a logger
         }
@@ -90,85 +83,9 @@ public class ServerView extends Observable implements Initializable, Observer{
         loader.setLocation(getClass().getResource(source));
         return loader;
     }
-    private HBox makeItemHBox(JFXButton button, JFXListView extras, JFXListView removed){
-        HBox itemBox = new HBox();
-        Region filler = new Region();
-        HBox.setHgrow(filler, Priority.ALWAYS);
-        MenuButton quantitySelector = new MenuButton("Select Quantity");
-        for (int i = 1; i < 11; i++) {
-            final int j = i;
-            MenuItem quantity = new MenuItem(String.valueOf(j));
-            quantity.setOnAction(e -> quantitySelector.setText(String.valueOf(j))); //will need more here
-            quantitySelector.getItems().add(quantity);
-        }
-        itemBox.getChildren().addAll(quantitySelector, filler, new Label(button.getText()));
-        itemBox.setOnMouseClicked(e -> loadModLists(Restaurant.getInstance().getMenu().getMenuItem(button.getText()), extras, removed));
-        return itemBox;
+    private void loadAddDialog(StackPane parent, List<JFXButton> selectedItemButtons){
+        orderDetailsPopup = new OrderDetailsPopup(parent, selectedItemButtons);
     }
 
-    private void loadModLists(menu.MenuItem item, JFXListView<JFXButton> extras, JFXListView<JFXButton> removed){
-        List<Ingredient> allIngredients = Restaurant.getInstance().getMenu().getAllIngredients();
-        allIngredients.removeAll(item.getIngredients());
-        ObservableList<JFXButton> addableIngredients = FXCollections.observableArrayList();
-        for(Ingredient i : allIngredients){
-            addableIngredients.add(new JFXButton(i.getName()));
-        }
-        extras.setItems(addableIngredients);
-
-        ObservableList<JFXButton> removeableIngredients = FXCollections.observableArrayList();
-        for (Ingredient i : item.getIngredients()){
-            removeableIngredients.add(new JFXButton(i.getName()));
-        }
-        removed.setItems(removeableIngredients);
-    }
-    private void loadAddDialog(List<JFXButton> selectedItemButtons){
-        //HEADER
-        JFXDialogLayout content = new JFXDialogLayout();
-        Label orderDetailsDialogHeader = new Label("Order Details");
-
-
-        VBox dialogRoot = new VBox(5);
-        JFXListView<JFXButton> extraIngredientsListView = new JFXListView<>();
-        JFXListView<JFXButton> removedIngredientsListView = new JFXListView<>();
-
-        VBox top = new VBox();
-        JFXListView<HBox> orderItemListView = new JFXListView<>();
-        for (JFXButton button : selectedItemButtons){
-            orderItemListView.getItems().add(makeItemHBox(button, extraIngredientsListView, removedIngredientsListView));
-        }
-        top.getChildren().add(orderItemListView);
-        HBox bottom = new HBox();
-        bottom.setMinSize(367, 355);
-        VBox left = new VBox();
-        left.setMinWidth(185);
-        left.getChildren().add(extraIngredientsListView);
-
-        VBox right = new VBox();
-        right.setMinWidth(185);
-        right.getChildren().add(removedIngredientsListView);
-
-        bottom.getChildren().addAll(left, right);
-
-
-
-
-        dialogRoot.getChildren().addAll(top, bottom);
-        JFXButton cancelButton = new JFXButton("Cancel");
-        JFXButton confirmButton = new JFXButton("Confirm Order");
-
-        content.setHeading(orderDetailsDialogHeader);
-        content.setBody(dialogRoot);
-        content.setActions(cancelButton, confirmButton);
-
-        JFXDialog dialog = new JFXDialog(serverViewStackPane, content, JFXDialog.DialogTransition.CENTER);
-        cancelButton.setOnAction(e -> dialog.close());
-        confirmButton.setOnAction(e -> {
-            //newOrder
-            dialog.close();
-        });
-        dialog.show();
-
-
-    }
 
 }
