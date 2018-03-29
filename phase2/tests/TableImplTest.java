37c30a9d0fcd65086344f58639174ee693df4b34
sanch187 <saquiles0@gmail.com>
2018-03-29 02:02:45 -0400

    Implemented testAddOrder and testAddOrderToBill.

--- phase2/tests/TableImplTest.java
+++ phase2/tests/TableImplTest.java
@@ -1,16 +1,41 @@
 // TODO: write these tests
 
+import menu.BurgerMenu;
+import menu.Menu;
 import org.junit.Test;
+import restaurant.Order;
 import restaurant.Restaurant;
+import restaurant.Table;
+import restaurant.TableImpl;
+
+import static junit.framework.TestCase.assertEquals;
+import static restaurant.OrderFactory.makeOrder;
 
 public class TableImplTest {
+    private static final Menu menu = new BurgerMenu();
+
     Restaurant r = Restaurant.getInstance(10, 0.13);
 
     @Test
-    public void testAddOrder(){}
+    public void testAddOrder(){
+        r.start();
+        Order o = makeOrder(menu.getMenu(), 1, 1, 1);
+        Table t = new TableImpl(1, false);
+        int currValue = t.getOrders().size();
+        t.addOrder(o);
+        assertEquals(currValue + 1, t.getOrders().size() );
+
+    }
 
     @Test
-    public void testAddOrderToBill(){}
+    public void testAddOrderToBill(){
+        r.start();
+        Order o = makeOrder(menu.getMenu(), 1, 1, 1);
+        Table t = new TableImpl(1, false);
+        t.addOrder(o);
+        t.addOrderToBill(o);
+        assertEquals(0, t.getOrders().size());
+    }
 
     @Test
     public void testRemoveFromBill(){}
