5ed14e568903ac8feabef5400b6ed1cb34ae17f8
sanch187 <saquiles0@gmail.com>
2018-03-29 01:33:34 -0400

    TableImplTest frame completed.

--- phase2/tests/TableImplTest.java
+++ phase2/tests/TableImplTest.java
@@ -1,5 +1,29 @@
 // TODO: write these tests
 
-public class TableImplTest {
+import org.junit.Test;
+import restaurant.Restaurant;
 
+public class TableImplTest {
+    Restaurant r = Restaurant.getInstance(10, 0.13);
+
+    @Test
+    public void testAddOrder(){}
+
+    @Test
+    public void testAddOrderToBill(){}
+
+    @Test
+    public void testRemoveFromBill(){}
+
+    @Test
+    public void testAddSeat(){}
+
+    @Test
+    public void testRemoveSeat(){}
+
+    @Test
+    public void testJoinCheques(){}
+
+    @Test
+    public void testResetTable(){}
 }
